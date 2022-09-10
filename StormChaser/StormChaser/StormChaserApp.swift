f65a93f582efe6b8f10b1a3953bd6b00998ecbee
Jeff Verkoeyen <jverkoey@gmail.com>
2022-09-10 12:36:55 -0700

    Get music player working.

--- StormChaser/StormChaser/StormChaserApp.swift
+++ StormChaser/StormChaser/StormChaserApp.swift
@@ -5,6 +5,7 @@
 //  Created by Jeff Verkoeyen on 9/8/22.
 //
 
+import AVKit
 import SwiftUI
 import SQLite
 import HurricaneDB
@@ -45,15 +46,19 @@ struct MediaItem: Hashable, Identifiable {
   let artist: String
   let rating: Int64
   let ratingComputed: Bool
+  let url: URL
 }
 
 struct PlaylistView: SwiftUI.View {
   private let playlist: Playlist
   private let mediaItems: [MediaItem]
-  @State private var selection: MediaItem?
+  @SwiftUI.Binding private var selectedItem: MediaItem?
+  private let audioPlayer: AVPlayer
 
-  fileprivate init(playlist: Playlist, db: Connection) {
+  fileprivate init(playlist: Playlist, selectedItem: SwiftUI.Binding<MediaItem?>, audioPlayer: AVPlayer, db: Connection) {
     self.playlist = playlist
+    self._selectedItem = selectedItem
+    self.audioPlayer = audioPlayer
 
     if !playlist.items.isEmpty {
       let playlistOrder = playlist.items.components(separatedBy: ",").map { Int64(bitPattern: UInt64($0, radix: 16)!) }
@@ -67,10 +72,14 @@ struct PlaylistView: SwiftUI.View {
             MediaItemTable.title,
             MediaItemTable.artistId,
             MediaItemTable.rating,
-            MediaItemTable.ratingComputed
+            MediaItemTable.ratingComputed,
+            MediaItemTable.location
           )
           .where( playlistOrder.contains(MediaItemTable.id) )
       ).reduce(into: [:], { partialResult, row in
+        guard let location = row[MediaItemTable.location] else {
+          return
+        }
         let artistName: String?
         if let artistId = row[MediaItemTable.artistId] {
           if let name = artistNames[artistId] {
@@ -90,7 +99,8 @@ struct PlaylistView: SwiftUI.View {
           title: row[MediaItemTable.title],
           artist: artistName ?? "",
           rating: row[MediaItemTable.rating],
-          ratingComputed: row[MediaItemTable.ratingComputed]
+          ratingComputed: row[MediaItemTable.ratingComputed],
+          url: URL(string: location)!
         )
       })
 
@@ -101,7 +111,7 @@ struct PlaylistView: SwiftUI.View {
   }
 
   var body: some SwiftUI.View {
-    List(mediaItems, id: \.self, selection: $selection) { item in
+    List(mediaItems, id: \.self, selection: $selectedItem) { item in
       HStack {
         Text(String(item.rating))
           .frame(minWidth: 50)
@@ -117,6 +127,26 @@ struct PlaylistView: SwiftUI.View {
   }
 }
 
+enum Utility {
+  static func formatSecondsToHMS(_ seconds: TimeInterval) -> String {
+    let secondsInt:Int = Int(seconds.rounded(.towardZero))
+
+    let dh: Int = (secondsInt/3600)
+    let dm: Int = (secondsInt - (dh*3600))/60
+    let ds: Int = secondsInt - (dh*3600) - (dm*60)
+
+    let hs = "\(dh > 0 ? "\(dh):" : "")"
+    let ms = "\(dm<10 ? "0" : "")\(dm):"
+    let s = "\(ds<10 ? "0" : "")\(ds)"
+
+    return hs + ms + s
+  }
+}
+
+class AudioPlayerObserver {
+  var audioPlayerObserver: Any?
+}
+
 @main
 struct StormChaserApp: App {
   private let documentsUrl = getDocumentsDirectory()
@@ -124,8 +154,11 @@ struct StormChaserApp: App {
   private let playlists: [Playlist]
 
   @State private var playingItem: MediaItem?
+  @State private var selectedItem: MediaItem?
   @State private var selection: Playlist?
-  @State private var volume: Double = 1.0
+  private let audioPlayer = AVPlayer()
+  @State private var currentTime: TimeInterval = 0
+  @State private var currentDuration: TimeInterval = 0
 
   init() {
     self.db = try! Connection(documentsUrl.appendingPathComponent("hurricane.sqlite3").absoluteString)
@@ -170,6 +203,8 @@ struct StormChaserApp: App {
     })
   }
 
+  let audioPlayerObserver = AudioPlayerObserver()
+
   var body: some Scene {
     WindowGroup {
       NavigationView {
@@ -180,23 +215,82 @@ struct StormChaserApp: App {
         VStack {
           HStack {
             Button {
-              print("Play button pressed")
+              playingItem = selectedItem
+
+              currentDuration = 0
+              if let playedItem = playingItem {
+                audioPlayer.pause()
+                let playerItem = AVPlayerItem(url: playedItem.url)
+                audioPlayer.replaceCurrentItem(with: playerItem)
+                audioPlayer.play()
+
+                if audioPlayerObserver.audioPlayerObserver == nil {
+                  let interval = CMTime(seconds: 0.1, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+                  audioPlayerObserver.audioPlayerObserver = audioPlayer.addPeriodicTimeObserver(forInterval: interval, queue: .main) { time in
+                    currentTime = time.seconds
+                    currentDuration = audioPlayer.currentItem!.duration.seconds
+                    if currentDuration.isNaN {
+                      currentDuration = 0
+                    }
+                  }
+                }
+              } else {
+                if let audioPlayerObserver = audioPlayerObserver.audioPlayerObserver {
+                  audioPlayer.removeTimeObserver(audioPlayerObserver)
+                  self.audioPlayerObserver.audioPlayerObserver = nil
+                }
+                audioPlayer.pause()
+              }
             } label: {
-              Image(systemName: "play.fill")
+              if playingItem == nil {
+                Image(systemName: "play.fill")
+              } else {
+                Image(systemName: "pause.fill")
+              }
             }
-            .disabled(playingItem == nil)
+            .disabled(selectedItem == nil && playingItem == nil)
             if let playingItem = playingItem {
               Text(playingItem.title)
             } else {
               Text("No track playing")
             }
-            Slider(value: $volume)
-              .frame(maxWidth: 150)
+            Slider(value: $currentTime,
+                   in: 0...currentDuration,
+                   onEditingChanged: sliderEditingChanged,
+                   minimumValueLabel: Text("\(Utility.formatSecondsToHMS(currentTime))"),
+                   maximumValueLabel: Text("\(Utility.formatSecondsToHMS(currentDuration))")) {
+            }
           }
           if let selection = self.selection {
-            PlaylistView(playlist: selection, db: db)
+            PlaylistView(playlist: selection, selectedItem: $selectedItem, audioPlayer: audioPlayer, db: db)
           } else {
             Text("Pick a playlist")
+          }
+        }
+      }
+    }
+  }
+
+  private func sliderEditingChanged(editingStarted: Bool) {
+    if editingStarted {
+      // Tell the PlayerTimeObserver to stop publishing updates while the user is interacting
+      // with the slider (otherwise it would keep jumping from where they've moved it to, back
+      // to where the player is currently at)
+      if let audioPlayerObserver = audioPlayerObserver.audioPlayerObserver {
+        audioPlayer.removeTimeObserver(audioPlayerObserver)
+        self.audioPlayerObserver.audioPlayerObserver = nil
+      }
+    }
+    else {
+      // Editing finished, start the seek
+      let targetTime = CMTime(seconds: currentTime, preferredTimescale: 600)
+      audioPlayer.seek(to: targetTime) { _ in
+        let interval = CMTime(seconds: 0.1, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+        audioPlayerObserver.audioPlayerObserver = audioPlayer.addPeriodicTimeObserver(forInterval: interval, queue: .main) { time in
+          currentTime = time.seconds
+          currentDuration = audioPlayer.currentItem!.duration.seconds
+          if currentDuration.isNaN {
+            currentDuration = 0
           }
         }
       }
