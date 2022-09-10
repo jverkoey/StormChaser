6469e0c98164c6bd2e9710a3dd3db7e41c197f9b
Jeff Verkoeyen <jverkoey@gmail.com>
2022-09-10 00:25:25 -0700

    Move 1/2-star tracks over to Dropbox.

--- Hurricane/Sources/Hurricane/main.swift
+++ Hurricane/Sources/Hurricane/main.swift
@@ -16,6 +16,30 @@ private let db = try Connection(dbUrl.absoluteString)
 
 //try migrateItunesToDatabase(db: db)
 
+let library = try buildInMemoryRepresentation(db: db)
+
+// Move 1/2-star tracks over to Dropbox
+//let fm = FileManager.default
+//for (_, track) in library.tracks.filter { !$0.value.ratingComputed && $0.value.rating == 20 } {
+//  let url = URL(string: track.location)!
+//  if fm.fileExists(atPath: url.path), url.path.starts(with: "/Volumes/CHOCOBO/") {
+//    let dropboxPath = url.path.replacingOccurrences(of: "/Volumes/CHOCOBO/", with: "/Users/featherless/Dropbox/Chocobo-1star/")
+//    print("Moving \(url.path) to \(dropboxPath)...")
+//    try fm.createDirectory(atPath: URL(fileURLWithPath: dropboxPath).deletingLastPathComponent().path, withIntermediateDirectories: true)
+//    try fm.moveItem(atPath: url.path, toPath: dropboxPath)
+//  }
+//}
+//
+//for (_, track) in library.tracks.filter { !$0.value.ratingComputed && $0.value.rating == 40 } {
+//  let url = URL(string: track.location)!
+//  if fm.fileExists(atPath: url.path), url.path.starts(with: "/Volumes/CHOCOBO/") {
+//    let dropboxPath = url.path.replacingOccurrences(of: "/Volumes/CHOCOBO/", with: "/Users/featherless/Dropbox/Chocobo-2star/")
+//    print("Moving \(url.path) to \(dropboxPath)...")
+//    try fm.createDirectory(atPath: URL(fileURLWithPath: dropboxPath).deletingLastPathComponent().path, withIntermediateDirectories: true)
+//    try fm.moveItem(atPath: url.path, toPath: dropboxPath)
+//  }
+//}
+
 //try writeDatabaseToITunesXML(db: db) { track in
 //  return !track.ratingComputed && track.rating >= 60
 //}
