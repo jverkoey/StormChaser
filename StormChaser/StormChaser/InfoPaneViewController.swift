7802f213b403d621759c1561087c73ce2b3a966e
Jeff Verkoeyen <jverkoey@gmail.com>
2022-09-23 21:47:47 -0700

    Add support for copy.

--- StormChaser/StormChaser/InfoPaneViewController.swift
+++ StormChaser/StormChaser/InfoPaneViewController.swift
@@ -7,6 +7,11 @@
 
 import UIKit
 
+private enum CellTypes: String {
+  case editableText
+  case label
+}
+
 final class InfoPaneViewController: UIViewController {
   let model: Model
   init(model: Model) {
@@ -27,7 +32,10 @@ final class InfoPaneViewController: UIViewController {
     tableView = UITableView(frame: view.bounds, style: .insetGrouped)
     tableView.translatesAutoresizingMaskIntoConstraints = false
     tableView.dataSource = self
-    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    tableView.delegate = self
+
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: CellTypes.editableText.rawValue)
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: CellTypes.label.rawValue)
 
     view.addSubview(tableView)
 
@@ -47,32 +55,78 @@ final class InfoPaneViewController: UIViewController {
       } else {
         mediaItem = nil
       }
-
-      tableView.reloadData()
     }
   }
 
   var mediaItem: MediaItem? {
     didSet {
-      print(mediaItem)
+      if isViewLoaded {
+        tableView.reloadData()
+      }
     }
   }
 }
 
 extension InfoPaneViewController: UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 2
+  }
+
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return 1
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-    var config = cell.textFieldConfiguration()
-    config.text = mediaItem?.title
-    cell.contentConfiguration = config
-    return cell
+
+    switch indexPath.section {
+    case 0:
+      let cell = tableView.dequeueReusableCell(withIdentifier: CellTypes.editableText.rawValue, for: indexPath)
+      var config = cell.textFieldConfiguration()
+      config.text = mediaItem?.title
+      cell.contentConfiguration = config
+      return cell
+
+    case 1:
+      let cell = tableView.dequeueReusableCell(withIdentifier: CellTypes.editableText.rawValue, for: indexPath)
+      var config = cell.defaultContentConfiguration()
+      config.text = mediaItem?.url?.path
+      cell.contentConfiguration = config
+      return cell
+
+    default:
+      break
+    }
+
+    return UITableViewCell(frame: .zero)
   }
 
   func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-    return "Title"
+    switch section {
+    case 0:
+      return "Title"
+    case 1:
+      return "Location"
+    default:
+      return nil
+    }
+  }
+}
+
+extension InfoPaneViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+    return false
+  }
+
+  func tableView(_ tableView: UITableView, contextMenuConfigurationForRowAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+    let actionProvider: UIContextMenuActionProvider = { _ in
+      return UIMenu(title: "", children: [
+        UIAction(title: "Copy") { _ in
+          let pasteBoard = UIPasteboard.general
+          pasteBoard.string = self.mediaItem?.url?.path
+        }
+      ])
+    }
+
+    return UIContextMenuConfiguration(identifier: "unique-ID" as NSCopying, previewProvider: nil, actionProvider: actionProvider)
   }
 }
